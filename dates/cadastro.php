a10b3d88a8c91331d56b1b88ab167fe1c91c9cc2
RaphaelCarrah <raphaelscarrah95@gmail.com>
2017-01-22 04:14:27 -0300

    Criando cadastro no MySql

--- dates/cadastro.php
+++ dates/cadastro.php
@@ -24,35 +24,41 @@
 		mysql_query('SET character_set_client=utf8');
 		mysql_query('SET character_set_results=utf8');
 		$sql = "INSERT INTO usuarios(nome,cargo,setor,matricula,sexo,email,nasc) VALUES ('$nome','$cargo','$setor','$matricula','$sexo','$email','$nasc')";
+		
 		utf8_encode($sql);
 		
 		mysql_db_query('solicitacao_cadastro', $sql,$strcon) or die ("Não foi possivel registrar");
 		mysql_close($strcon);
-		echo "Chamado registrado";
-	 ?>
+		echo "Usuário Cadastrado com Sucesso";
+
+	
+?>
 	<div class="container">
 	 <table class="tabela">
-	 	<tr>
-	 		<th>Nome</th>
-	 		<th>Cargo</th>
-	 		<th>Setor</th>
-	 		<th>Matricula</th>
-	 		<th>Sexo</th>
-	 		<th>Email</th>
-	 		<th>Idade</th>
-	 	</tr>
-	 	<tr class="head">
-	 		<th><?php echo $nome ?></th>
-	 		<th><?php echo $cargo?></th>
-	 		<th><?php echo $setor ?></th>
-	 		<th><?php echo $matricula?></th>
-	 		<th><?php echo $sexo ?></th>
-	 		<th><?php echo $email?></th>
-	 		<th><?php echo $idade ?></th>
-	 	</tr>
+	 	 	<tr>
+		 		<th>Nome</th>
+		 		<th>Cargo</th>
+		 		<th>Setor</th>
+		 		<th>Matricula</th>
+		 		<th>Sexo</th>
+		 		<th>Email</th>
+		 		<th>Idade</th>
+		 	</tr>
+		 	
 
-	 </table>
+			<tr>
+				<th><?echo $nome?></th>
+				<th><?echo $cargo?></th>
+				<th><?echo $setor?></th>
+				<th><?echo $matricula?></th>
+				<th><?echo $sexo?></th>
+				<th><?echo $email?></th>
+				<th><?echo $idade?></th>
+
+			</tr>
+	 	</table>
 	 </div>
+
 	 <br><br>
 	 <div class="button">
 	 <input type="button" class="buttonsend" value="Voltar" onClick="history.go(-1)"> 
